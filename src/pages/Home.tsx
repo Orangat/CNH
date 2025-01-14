2c9925244f47496c8b8ae5d3e6d8438ebe2cbf42
Serhii Nazarov <dr.orangat@gmail.com>
2025-01-13 23:33:27 -0500

    Added plugin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import UpcomingServices from '../components/UpcompingServices';
 import AboutChurchHome from '../components/AboutChurchHome';
 import SmallGroups from '../components/SmallGroups';
+import { useLocation } from 'react-router-dom';
+
 
 const Home: React.FC = () => {
   const videoUrl = './assets/bg_video.mp4';
+  const location = useLocation();
 
   return (
     <div className="Home">
-      <UpcomingServices videoUrl={videoUrl}>
+      <UpcomingServices key={location.pathname} videoUrl={videoUrl}>
         <h1 className="overlay-title">SUNDAYS</h1>
         <p>Join us for services at<br /> 9:30AM (Ukrainian) <br /> 12:00PM (English)</p>
       </UpcomingServices>
