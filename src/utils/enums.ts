4e4a65dc318cbecf73c7360f5f5f6ecae76131be
Serhii Nazarov <dr.orangat@gmail.com>
2026-03-06 13:00:24 -0500

    changed phone number

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -2,5 +2,5 @@ export enum ChurchInformation {
 	ENGLISH_SERVICE_TIME = '10:00 AM',
 	UKRAINIAN_SERVICE_TIME = '12:00 PM',
 	CHURCH_ADDRESS = '13601 Idlewild Rd, Matthews, NC 28105',
-	CHURCH_PHONE = '(704) 453-9365'
+	CHURCH_PHONE = '+1 (704) 609-7110'
 }
